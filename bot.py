257085005d48266d5f0e4ae0cdfd60fbd38acef7
yamato3042 <quentinvalour42@gmail.com>
2024-10-09 10:57:22 +0200

    Bot supression des tokens

--- bot.py
+++ bot.py
@@ -24,6 +24,15 @@ def updateNotes(cursor: psycopg2.extensions.cursor):
     for i in notes.keys():
         cursor.execute("UPDATE fics SET note = %s WHERE id = %s", (int(notes[i]),i,))
         
+def clean_shorts_tokens(cursor:psycopg2.extensions.cursor):
+    #On nettoie le bordel
+    print("Nettoyage des tokens utilisateurs temporaires")
+    cursor.execute("DELETE FROM users_shorts_tokens WHERE creation < NOW() - INTERVAL '1 day';")
+
+def clean_tokens(cursor: psycopg2.extensions.cursor):
+    #On nettoie les tokens en httponly, ceux pas utilisés depuis 6 mois
+    print("Nettoyage des tokens utilisateurs")
+    cursor.execute("DELETE FROM users_token WHERE lastconn < NOW() - INTERVAL '6 months';")
     
 conn = psycopg2.connect(database="noelfic",
                         host="localhost",
@@ -33,6 +42,8 @@ conn = psycopg2.connect(database="noelfic",
 cursor = conn.cursor()
 
 updateNotes(cursor)
+clean_tokens(cursor)
+clean_shorts_tokens(cursor)
 
 conn.commit()
 conn.close()
